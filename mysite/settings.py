ec09e92be5793bd986e566ceb6a1bc0c09f26da0
petbell <petbell@live.com>
2024-05-17 15:12:16 +0100

    Allowed host changed to all

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-89=r%nowzid3&$8qc1*l37tu_gx1e&1vx*#qmn0l_-y4r%rhdn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
